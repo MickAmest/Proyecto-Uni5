7d37ad293207b2b7627ac01abfb5588dec85df82
Fouquieria Columnaris <cirio@node07.conabio.gob.mx>
2020-02-16 13:59:55 -0600

    modified get_data.sh

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -1,6 +1,8 @@
 ### Get vcf data from Dryad repository and save then to data directory
 ### Miguel Amaro, Feb 2020
 
-cd ..
+cd ../
 mkdir data
+cd data
 wget https://datadryad.org/stash/downloads/file_stream/6226
+mv 6226 wolves.vcf 
