6f2b910386fe23f9fa47c563706cfd9d545b2b22
MickAmest <aejmiguel@outlook.com>
2020-02-16 13:43:58 -0600

    Modified get_data script

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -1,5 +1,6 @@
 ### Get vcf data from Dryad repository and save then to data directory
 ### Miguel Amaro, Feb 2020
 
-cd ../data
+cd ..
+mkdir data
 wget https://datadryad.org/stash/downloads/file_stream/6226
