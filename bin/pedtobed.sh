36a27a1c942fc268478da8ba45902348e7fe268b
MickAmest <aejmiguel@outlook.com>
2020-02-24 14:34:59 -0600

    edited pedtobed.sh

--- bin/pedtobed.sh
+++ bin/pedtobed.sh
@@ -1,2 +1,3 @@
 ### Makes bed files from the VCFtools ped output
+### This script asumes that the plink executable is placed in the same bin directory as the script
 ./plink --file ../data/wolves_maf05 --allow-extra-chr --chr-set 38 --make-bed --out ../data/wolves_maf05 
